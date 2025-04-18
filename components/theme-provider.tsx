9148f5aca74285ae2534a3e485db3ac37702621d
Josh Glen <129418264+JG21243@users.noreply.github.com>
2025-04-18 19:19:11 +0000

    button and theme provider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,16 @@
+// components/theme-provider.tsx
 'use client'
 
 import * as React from 'react'
 import {
   ThemeProvider as NextThemesProvider,
+  useTheme as _useTheme,
   type ThemeProviderProps,
 } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider(props: ThemeProviderProps) {
+  return <NextThemesProvider {...props} />
 }
+
+// add this:
+export const useTheme = _useTheme
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
-const buttonVariants = cva(
+export const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
   {
     variants: {
